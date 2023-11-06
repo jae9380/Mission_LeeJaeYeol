6f2447eb348966e7b1b814e4255c1fe8452d52fc
jae9380 <ljy5314@gmail.com>
2023-11-06 10:51:59 +0900

    4단계

--- src/main/java/com/ll/quotation/Controller.java
+++ src/main/java/com/ll/quotation/Controller.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Controller {
     private Scanner sc;
+    int count=1;
 
     public Controller(Scanner sc){
         this.sc=sc;
@@ -13,7 +14,8 @@ public class Controller {
         String saying =sc.nextLine();
         System.out.print("작가 : ");
         String author=sc.nextLine();
-        System.out.println("1번 명언이 등록 되었습니다.");
+        System.out.printf("%d번 명언이 등록 되었습니다.\n",count);
+        count++;
     }
 
 }
